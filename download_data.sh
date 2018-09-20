b3173d229d3ec65847c1f43c18d639b76a2ee073
Dendi Suhubdy <suhubdyd@iro.umontreal.ca>
2018-09-20 14:13:21 -0400

    initial working commit

---
+++ download_data.sh
@@ -0,0 +1,6 @@
+wget http://ftp.tugraz.at/pub/feichtenhofer/tsfusion/data/ucf101_jpegs_256.zip.001
+wget http://ftp.tugraz.at/pub/feichtenhofer/tsfusion/data/ucf101_jpegs_256.zip.002
+wget http://ftp.tugraz.at/pub/feichtenhofer/tsfusion/data/ucf101_jpegs_256.zip.003
+
+cat ucf101_jpegs_256.zip* > ucf101_jpegs_256.zip
+unzip ucf101_jpegs_256.zip
